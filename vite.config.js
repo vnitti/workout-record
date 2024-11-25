623500b0ad656a36f3beaf9dc0a638701ba102d1
vnitti <vicentenitti8@gmail.com>
2024-11-25 00:22:38 -0300

    minor updates in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
-})
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+});
